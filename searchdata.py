7d4f8591374f9b3ac06c3340cf174fef5b85c2bd
Xuan Zheng <eagledude200@gmail.com>
2023-10-16 18:19:54 -0400

    Update searchdata.py

--- searchdata.py
+++ searchdata.py
@@ -1,2 +1,22 @@
 def get_outgoing_links(URL):
-    temp
+    return None
+
+
+def get_incoming_links(URL):
+    return None
+
+
+def get_page_rank(URL):
+    return None
+
+
+def get_idf(URL):
+    return None
+
+
+def get_tf(URL, word):
+    return None
+
+
+def get_tf_idf(URL, word):
+    return None
