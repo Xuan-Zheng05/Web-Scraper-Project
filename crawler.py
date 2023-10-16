d9d45958c7c87e413c17289be91f796dec7bb078
Xuan Zheng <91241583+Xuan-Zheng05@users.noreply.github.com>
2023-10-16 19:12:22 -0400

    update

--- crawler.py
+++ crawler.py
@@ -2,14 +2,19 @@ import webdev
 import os
 
 
-# class that stores all nessecary information for a url
+# class that stores all necessary information for a url
 class urlData:
     name = ""
     content = dict()
     outgoingLinks = []
     incomingLinks = set()
+    pagerank = -1
+    tf = -1
+    idf = -1
+    tfIdf = -1
 
 
+urlQueue = []
 urlUsed = set()
 allUrlData = dict()
 
@@ -17,65 +22,75 @@ allUrlData = dict()
 def crawl(seed):
     # TODO delete all stuff before the crawl
 
-    contents = webdev.read_url(seed)
+    #adds seed as first URL
+    urlQueue.append(seed)
+    urlUsed.add(seed)
 
-    # checks if the current url has already been initalized as an object
-    if seed in allUrlData:
-        currUrlData = allUrlData[seed]
-    else:
-        currUrlData = urlData()
+    while len(urlQueue) > 0:
+        currUrl = urlQueue[0]
+        urlQueue.pop()
 
-    # gets the name for the current URL
-    titleIndex = contents.index("<title>")
-    titleEnd = contents.index("</title>")
-    title = contents[titleIndex + 7 : titleEnd]
-    currUrlData.name = title
+        contents = webdev.read_url(currUrl)
 
-    # gets all words from the current URL and organizes them into a dictionary
-    words = dict()
-    startP = [i for i in range(len(contents)) if contents.startswith("<p>", i)]
-    endP = [i for i in range(len(contents)) if contents.startswith("</p>", i)]
-    for i in range(len(startP)):
-        allWords = contents[startP[i] + 4 : endP[i]]
-        wordList = allWords.split()
-        for word in wordList:
-            if word not in words:
-                words[word] = 1
-            else:
-                words[word] = words[word] + 1
-    currUrlData.content = words
+        # checks if the current url has already been initalized as an object
+        if currUrl in allUrlData:
+            currUrlData = allUrlData[currUrl]
+        else:
+            currUrlData = urlData()
 
-    # gets all links from the current URL and also crawls through them
-    # furthermore also finds outgoing and incoming links
-    outgoingLinks = []
-    delUrlIndex = seed.rindex("/")
-    blankUrl = seed[:delUrlIndex]
-    instances = [i for i in range(len(contents)) if contents.startswith("href=", i)]
-    for index in instances:
-        url = ""
-        i = index + 6
-        while contents[i] != '"':
-            url += contents[i]
-            i += 1
-        if url[0] == ".":
-            url = blankUrl + url[1:]
-        if url not in urlUsed:
+        # gets the name for the current URL
+        titleIndex = contents.index("<title>")
+        titleEnd = contents.index("</title>")
+        title = contents[titleIndex + 7 : titleEnd]
+        currUrlData.name = title
+
+        # gets all words from the current URL and organizes them into a dictionary
+        words = dict()
+        startP = [i for i in range(len(contents)) if contents.startswith("<p>", i)]
+        endP = [i for i in range(len(contents)) if contents.startswith("</p>", i)]
+        for i in range(len(startP)):
+            allWords = contents[startP[i] + 4 : endP[i]]
+            wordList = allWords.split()
+            for word in wordList:
+                if word not in words:
+                    words[word] = 1
+                else:
+                    words[word] = words[word] + 1
+        currUrlData.content = words
+
+        # gets all links from the current URL and adds them to the queue
+        # furthermore also finds outgoing and incoming links
+        outgoingLinks = []
+        delUrlIndex = currUrl.rindex("/")
+        blankUrl = currUrl[:delUrlIndex]
+        instances = [i for i in range(len(contents)) if contents.startswith("href=", i)]
+        for index in instances:
+            url = ""
+            i = index + 6
+            while contents[i] != '"':
+                url += contents[i]
+                i += 1
+            if url[0] == ".":
+                url = blankUrl + url[1:]
+            
+            if url not in urlUsed:
+                urlQueue.append(url)
             urlUsed.add(url)
-            if seed != url:
+            
+            if currUrl != url:
                 if url in allUrlData:
                     incomingUrl = allUrlData[url]
                 else:
                     incomingUrl = urlData()
-                incomingUrl.incomingLinks.add(seed)
+                incomingUrl.incomingLinks.add(currUrl)
                 allUrlData[url] = incomingUrl
 
-            crawl(url)
 
-        # this is for outgoing and incoming URLs
-        outgoingLinks.append(url)
+            # this is for outgoing and incoming URLs
+            outgoingLinks.append(url)
 
-    currUrlData.outgoingLinks = outgoingLinks
-    allUrlData[seed] = currUrlData
+        currUrlData.outgoingLinks = outgoingLinks
+        allUrlData[currUrl] = currUrlData
     return len(urlUsed)
 
 
